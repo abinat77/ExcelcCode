b1c05bd7734f1ce52f43498162928b24d2bfa7aa
abinat77 <53835397+abinat77@users.noreply.github.com>
2020-04-01 11:53:46 -0400

    Add files via upload

---
+++ src/FileUtils.java
@@ -0,0 +1,59 @@
+import java.io.FileInputStream;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
+import org.testng.annotations.Test;
+
+
+public class FileUtils {
+	
+	 static XSSFSheet sheet= null;
+	final static String path ="C:\\Users\\User\\cucucmberWorkshop\\Excel\\book.xlsx";
+	
+	
+	@Test
+	public static void test1() throws IOException, Exception, Throwable {
+	 writeData( "Sheet1", 2, "RollNo", "001	");
+		}
+
+
+	public static void writeData( String sheetName, int rowNum, String cellNum, String value) throws IOException, EncryptedDocumentException, OpenXML4JException{
+		
+		FileInputStream fis = new FileInputStream(path);
+		Workbook wb = WorkbookFactory.create(fis);
+		 int col_Num = -1;
+         sheet = (XSSFSheet) wb.getSheet(sheetName);
+
+          Row row = sheet.getRow(0);
+         for (int i = 0; i < row.getLastCellNum(); i++) {
+             if (row.getCell(i).getStringCellValue().trim().equals(cellNum))
+             {
+                 col_Num = i;
+             }
+         }
+         sheet.autoSizeColumn(col_Num);
+         row = sheet.getRow(rowNum - 1);
+         if(row==null)
+             row = sheet.createRow(rowNum - 1);
+
+         Cell cell = row.getCell(col_Num);
+         if(cell == null)
+             cell = row.createCell(col_Num);
+
+         cell.setCellValue(value);
+         FileOutputStream fos = new FileOutputStream(path);
+         wb.write(fos);
+         fos.close();
+       
+        
+     }
+
+}
